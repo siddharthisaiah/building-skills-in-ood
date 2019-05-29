5a7b4e1bf39daef158a0936cb62df882bd1f4b0f
Siddharth <siddharthisaiah@gmail.com>
2019-05-30 01:46:17 +0530

    bin class and tests for bin

--- roulette.py
+++ roulette.py
@@ -27,5 +27,15 @@ class Outcome:
 
 
 
-
+class Bin(frozenset):
+    """
+    Bin contains a collection of Outcomes which reflect the winning bets 
+    that are paid for a particular bin on a Roulette wheel. 
+    In Roulette, each spin of the wheel has a number of Outcomes. 
+    Example: A spin of 1, selects the “1” bin with the following winning 
+    Outcomes: “1” , “Red” , “Odd” , “Low” , “Column 1” , “Dozen 1-12” , 
+    “Split 1-2” , “Split 1-4” , “Street 1-2-3” , “Corner 1-2-4-5” , “Five Bet” , 
+    “Line 1-2-3-4-5-6” , “00-0-1-2-3” , “Dozen 1” , “Low” and “Column 1” . 
+    These are collected into a single Bin .
+    """
         
--- roulette_test.py
+++ roulette_test.py
@@ -1,5 +1,5 @@
 import unittest
-from roulette import Outcome
+from roulette import Outcome, Bin
 
 
 class OutcomeTest(unittest.TestCase):
@@ -23,6 +23,23 @@ class OutcomeTest(unittest.TestCase):
     def test_outcome_pays_correct_win_amount(self):
         self.assertEqual(self.o3.winAmount(10), 350)
 
+
+class BinTest(unittest.TestCase):
+
+    def setUp(self):
+        self.five = Outcome("00-0-1-2-3", 6)
+        self.three_five = Outcome("3-5", 17)
+        self.one_twelve = Outcome("1-12", 2)
+        self.low = Outcome("low-1-18", 1)
+        self.high = Outcome("high-19-36", 1)
+        self.street_thirty_one_two_three = Outcome("31-32-33", 11)
+        
+
+    def test_bins_can_be_created_from_outcomes(self):
+        self.assertEqual(Bin([self.five, self.three_five, self.one_twelve]), Bin({Outcome("00-0-1-2-3", 6), Outcome("3-5", 17), Outcome("1-12", 2)}) )
+        self.assertEqual(Bin([Outcome("0",35), self.five]), Bin({Outcome("0",35), Outcome("00-0-1-2-3", 6)}))
+        self.assertEqual(Bin([Outcome("00",35), self.five]), Bin({Outcome("00",35), Outcome("00-0-1-2-3", 6)}))
+    
 
 if __name__ == '__main__':
     unittest.main()
